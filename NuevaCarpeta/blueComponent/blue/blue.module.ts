a266b1427c0012973cb11bd26ab360e7034d28dc
berny13 <57494751+berny13@users.noreply.github.com>
2020-08-28 11:20:03 +0200

    se cargan componentes blue

---
+++ NuevaCarpeta/blueComponent/blue/blue.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BlueComponent } from './blue.component';
+
+
+
+@NgModule({
+  declarations: [BlueComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [BlueComponent]
+})
+export class BlueModule { }
